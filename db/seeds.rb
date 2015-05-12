3020f526b7aa531da873830121b78daa2017132b
Laurence Drake <laurencedrake@gmail.com>
2015-05-12 11:36:17 -0500

    Create Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!({email: "user1@example.com",
+              password: "password",
+              password_confirmation: "password"})
+            .notes.create!({title: "Books I want to Read",
+                            content: "Book #1 by Author #1"})
+User.create!({email: "user2@example.com",
+              password: "password",
+              password_confirmation: "password"})
+            .notes.create!({title: "Shopping List",
+                            content: "Get Food"})
+User.create!({email: "user3@example.com",
+              password: "password",
+              password_confirmation: "password"})
+            .notes.create!({title: "Fix Car",
+                            content: "Take car to Bob "})
