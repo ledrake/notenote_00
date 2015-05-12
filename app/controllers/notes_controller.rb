638093e579e4ee5a7bbe7e7ed66bf443dedc9731
Laurence Drake <laurencedrake@gmail.com>
2015-05-12 11:35:59 -0500

    Add order clause to NotesController

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,7 @@ class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
   def index
-    @notes = Note.all
+    @notes = Note.all.order("created_at DESC")
   end
 
   def show
